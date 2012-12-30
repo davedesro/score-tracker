d59521d83510601fc8d5a059de279dd98e093c91
Dave Desrochers <ddesrochers@morseproject.com>
2012-12-30 10:27:48 -0500

    Inintial Checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoreTracker::Application.config.secret_token = 'a8185f2ab96e555eed3e222e53738a91d5d030d85fbbc7cbd635c755e591e4e83e96b3cedad116725e993e5a2b3db753095c0ac8672df479ced971ab96687252'
